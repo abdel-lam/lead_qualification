9c351ac373635604c80b511d2d4ebe5a2553d279
abdel-lam <lamrabtia20@gmail.com>
2022-05-14 23:47:51 +0100

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Lead Qualification',
+    'author': 'lamrabti abdellatif',
+    'summary': '',
+    'description': "",
+    'website': '',
+    'depends': ['crm'],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
